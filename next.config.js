3a0cb23fa14c768c344bd7cd3b6ca90d6e2712c7
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2021-11-29 23:22:01 +0700

    fixbuild config webpack

--- next.config.js
+++ next.config.js
@@ -4,5 +4,6 @@ module.exports = {
   env: {
     MONGO_URI: "mongodb://tioramadhn:mongodb@cluster0-shard-00-00.gduwf.mongodb.net:27017,cluster0-shard-00-01.gduwf.mongodb.net:27017,cluster0-shard-00-02.gduwf.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-8m45f7-shard-0&authSource=admin&retryWrites=true&w=majority"
   },
+  future: { webpack5: true, },
   concurrentFeatures: false
 }
